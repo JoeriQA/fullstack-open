f0d7648f9a161077d6b4e5eb5a333ed6196a4436
jsengers <j.sengers1@vgz.nl>
2026-03-12 16:02:06 +0100

    refactor courseinfo app parts 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = ({ course }) => {
+  const total = course.parts.reduce((acc, part) => {
+    return acc + part.exercises;
+  }, 0);
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total} />
+    </div>
+  );
+};
+
+export default Course;
